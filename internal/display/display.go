4d8d701b6f918ee8650806e5122d237a8dc9aca5
AKP <tom@tdpain.net>
2021-06-12 20:16:23 +0100

    Manage input

--- internal/display/display.go
+++ internal/display/display.go
@@ -3,22 +3,23 @@ package display
 import (
 	"fmt"
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 	"image/color"
 )
 
 type Display struct {
 	width, height int
-	scale int
+	scale         int
 
-	nextDisplay *[32][64]bool
+	nextDisplay    *[32][64]bool
 	currentDisplay [32][64]bool
 }
 
 func NewDisplay(width, height, scale int) *Display {
 	d := &Display{
-		width: width,
+		width:  width,
 		height: height,
-		scale: scale,
+		scale:  scale,
 	}
 	return d
 }
@@ -51,7 +52,7 @@ func (d *Display) Layout(outsideWidth, outsideHeight int) (int, int) {
 }
 
 func (d *Display) Start() error {
-	ebiten.SetWindowSize(d.width * d.scale, d.height * d.scale)
+	ebiten.SetWindowSize(d.width*d.scale, d.height*d.scale)
 	ebiten.SetWindowTitle("Hello, World!")
 	return ebiten.RunGame(d)
 }
@@ -61,6 +62,33 @@ func (d *Display) PublishNewDisplay(inp [32][64]bool) {
 	fmt.Println("sent new display")
 }
 
+var inputTranslationTable = map[ebiten.Key]uint8{
+	ebiten.KeyDigit1: 0x01,
+	ebiten.KeyDigit2: 0x02,
+	ebiten.KeyDigit3: 0x03,
+	ebiten.KeyDigit4: 0x0C,
+	ebiten.KeyQ:      0x04,
+	ebiten.KeyW:      0x05,
+	ebiten.KeyE:      0x06,
+	ebiten.KeyR:      0x0D,
+	ebiten.KeyA:      0x07,
+	ebiten.KeyS:      0x08,
+	ebiten.KeyD:      0x09,
+	ebiten.KeyF:      0x0E,
+	ebiten.KeyZ:      0x0A,
+	ebiten.KeyX:      0x00,
+	ebiten.KeyC:      0x0B,
+	ebiten.KeyV:      0x0F,
+}
+
 func (d *Display) GetPressedKeys() []uint8 {
-	return nil
+	var o []uint8
+
+	for _, p := range inpututil.PressedKeys() {
+		if x, ok := inputTranslationTable[p]; ok {
+			o = append(o, x)
+		}
+	}
+
+	return o
 }
