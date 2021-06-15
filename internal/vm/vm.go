cea1bbdc8b29e6f8eca5b6c3edcd13b8e0c5c01b
AKP <tom@tdpain.net>
2021-06-15 11:27:33 +0100

    Refactor for ease of testing
    Signed-off-by: AKP <tom@tdpain.net>

--- internal/vm/vm.go
+++ internal/vm/vm.go
@@ -99,6 +99,179 @@ func decrement(v *uint8) {
 	}
 }
 
+func (c *Chip8) fetchNext() {
+	c.cir[0] = c.memory[c.pc]
+	c.cir[1] = c.memory[c.pc+1]
+	c.pc += 2
+}
+
+func (c *Chip8) tick() {
+	// FETCH
+	c.fetchNext()
+
+	if c.Debug {
+		fmt.Printf("DEBUG: pc:0x%04x cir:0x%04x\n", c.pc - 2, c.cir)
+		fmt.Print("       ")
+		for i := 0; i < 16; i += 1 {
+			fmt.Printf("v%x:%02x ", i, *c.getRegisterPointer(byte(i)))
+		}
+		fmt.Printf("\n       ir:%04x\n", c.ir)
+	}
+
+	// DECODE + EXECUTE
+	switch c.cir[0] & 0xF0 { // first four bits
+	case 0x00:
+
+		switch c.cir {
+		case [2]byte{0x00, 0xE0}:
+			// 00E0 - clear screen
+			c.clearScreen()
+		case [2]byte{0x00, 0xEE}:
+			// 00EE - subroutine return
+			c.subroutineReturn()
+		default:
+			panic(fmt.Errorf("UNHANDLED at %x: %x\n", c.pc, c.cir))
+		}
+
+	case 0x10:
+		// 1NNN - jump
+		c.jump()
+
+	case 0x20:
+		// 2NNN - subroutine call
+		c.subroutineCall()
+
+	case 0x30:
+		// 3XNN - skip one if register equal to constant
+		c.skipEqRegConst()
+
+	case 0x40:
+		// 4XNN - skip one if register not equal to constant
+		c.skipNotEqRegConst()
+
+	case 0x50:
+		// 5XY0 - skip one if registers equal
+		c.skipEqRegReg()
+
+	case 0x60:
+		// 6XNN - set VX to NN
+		c.setRegisterToConstant()
+
+	case 0x70:
+		// 7XNN - add NN to VX without setting carry flag
+		c.addConstantToRegister()
+
+	case 0x80:
+		// Logical and arithmetic instructions
+		switch c.cir[1] & 0x0F { // switch on the last four bits - eg 00000000 00001111
+		case 0x00:
+			// 8XY0 - set VX to VY
+			c.setRegisterToRegister()
+		case 0x01:
+			// 8XY1 - set VX to logical OR of VX and VY
+			c.setRegisterToLogicalOr()
+		case 0x02:
+			// 8XY2 - set VX to logical AND of VX and VY
+			c.setRegisterToLogicalAnd()
+		case 0x03:
+			// 8XY3 - set VX to logical XOR of VX and VY
+			c.setRegisterToLogicalXor()
+		case 0x04:
+			// 8XY4 - set VX to the sum of VX and VY then set the carry flag as appropriate
+			c.setRegisterToSum()
+		case 0x05:
+			// 8XY5 - set VX to VX - VY then set the carry flag as appropriate
+			c.setRegisterToDifferenceA()
+		case 0x06:
+			// 8XY6 - set VX to VY (if CopyRegistersOnShift), shift the value of VX 1 bit right and set VF to
+			// the bit shifted out
+			c.shiftRight()
+		case 0x07:
+			// 8XY7 - set VX to VY - VX then set the carry flag as appropriate
+			c.setRegisterToDifferenceB()
+		case 0x0E:
+			// 8XYE - set VX to VY (if CopyRegistersOnShift), shift the value of VX 1 bit left and set VF to the
+			// bit shifted out
+			c.shiftLeft()
+		default:
+			panic(fmt.Errorf("UNHANDLED at %x: %x\n", c.pc, c.cir))
+		}
+
+	case 0x90:
+		// 9XYN - skip one if registers not equal
+		c.skipNotEqRegReg()
+
+	case 0xA0:
+		// ANNN - set index register to constant
+		c.setIndexRegister()
+
+	case 0xB0:
+		// BNNN - set PC to NNN + V0 - if VariableOffsetRegister, BXNN - set PC to XNN + VX
+		c.jumpWithOffset()
+
+	case 0xC0:
+		// CXNN - generate a random byte, AND it with NN and store in VX
+		c.random()
+
+	case 0xD0:
+		// DXYN - display
+		c.display()
+
+	case 0xE0:
+		// Input
+		switch c.cir[1] {
+		case 0x9E:
+			// EX9E - skip one if key with the value stored in VX is pressed
+			c.skipIfKey()
+		case 0xA1:
+			// EXA1 - skip one if key with the value stored in VX is not pressed
+			c.skipIfNotKey()
+		default:
+			panic(fmt.Errorf("UNHANDLED at %x: %x\n", c.pc, c.cir))
+		}
+
+	case 0xF0:
+		switch c.cir[1] {
+		case 0x07:
+			// FX07 - set value of VX to the current value of the delay timer
+			c.getDelayTimer()
+		case 0x15:
+			// FX15 - set delay timer to value of VX
+			c.setDelayTimer()
+		case 0x18:
+			// FX18 - set sound timer to the value of VX
+			c.setSoundTimer()
+		case 0x0A:
+			// FX0A - blocks until a key is pressed. Stores that key's value in VX then continues.
+			c.getPressedKey()
+		case 0x1E:
+			// FX1E - adds the value of VX to the index register and set VF accordingly if the index register
+			// "overflows" above 0x0FFF
+			c.addToIndexRegister()
+		case 0x29:
+			// FX29 - set the index register to the address of the hex character in VX
+			c.getFontCharacter()
+		case 0x33:
+			// FX33 - take the value of VX, converts it to a denary number and the put each individual digit in
+			// the memory location specified by the index register + the digit number
+			c.convertToDecimal()
+		case 0x55:
+			// FX55 - store the value of each general purpose register from V0 to VX inclusive in consecutive
+			// memory addresses starting from the current value of the index register
+			c.storeMemory()
+		case 0x65:
+			// FX65 - loads the value of each general purpose register from V0 to VX inclusive from consecutive
+			// memory addresses starting from the current value of the index register
+			c.loadMemory()
+		default:
+			panic(fmt.Errorf("UNHANDLED at %x: %x\n", c.pc, c.cir))
+		}
+
+	default:
+		panic(fmt.Errorf("UNHANDLED at %x: %x\n", c.pc, c.cir))
+	}
+}
+
 func (c *Chip8) Run() {
 
 	programTicker := time.NewTicker(time.Second / time.Duration(c.clockSpeedHertz))
@@ -127,172 +300,7 @@ MAINLOOP:
 			// TODO: Make noise!
 		case <-programTicker.C:
 
-			// FETCH
-			c.cir[0] = c.memory[c.pc]
-			c.cir[1] = c.memory[c.pc+1]
-			c.pc += 2
-
-			if c.Debug {
-				fmt.Printf("DEBUG: pc:0x%04x cir:0x%04x\n", c.pc - 2, c.cir)
-				fmt.Print("       ")
-				for i := 0; i < 16; i += 1 {
-					fmt.Printf("v%x:%02x ", i, *c.getRegisterPointer(byte(i)))
-				}
-				fmt.Printf("\n       ir:%04x\n", c.ir)
-			}
-
-			// DECODE + EXECUTE
-			switch c.cir[0] & 0xF0 { // first four bits
-			case 0x00:
-
-				switch c.cir {
-				case [2]byte{0x00, 0xE0}:
-					// 00E0 - clear screen
-					c.clearScreen()
-				case [2]byte{0x00, 0xEE}:
-					// 00EE - subroutine return
-					c.subroutineReturn()
-				default:
-					panic(fmt.Errorf("UNHANDLED at %x: %x\n", c.pc, c.cir))
-				}
-
-			case 0x10:
-				// 1NNN - jump
-				c.jump()
-
-			case 0x20:
-				// 2NNN - subroutine call
-				c.subroutineCall()
-
-			case 0x30:
-				// 3XNN - skip one if register equal to constant
-				c.skipEqRegConst()
-
-			case 0x40:
-				// 4XNN - skip one if register not equal to constant
-				c.skipNotEqRegConst()
-
-			case 0x50:
-				// 5XY0 - skip one if registers equal
-				c.skipEqRegReg()
-
-			case 0x60:
-				// 6XNN - set VX to NN
-				c.setRegisterToConstant()
-
-			case 0x70:
-				// 7XNN - add NN to VX without setting carry flag
-				c.addConstantToRegister()
-
-			case 0x80:
-				// Logical and arithmetic instructions
-				switch c.cir[1] & 0x0F { // switch on the last four bits - eg 00000000 00001111
-				case 0x00:
-					// 8XY0 - set VX to VY
-					c.setRegisterToRegister()
-				case 0x01:
-					// 8XY1 - set VX to logical OR of VX and VY
-					c.setRegisterToLogicalOr()
-				case 0x02:
-					// 8XY2 - set VX to logical AND of VX and VY
-					c.setRegisterToLogicalAnd()
-				case 0x03:
-					// 8XY3 - set VX to logical XOR of VX and VY
-					c.setRegisterToLogicalXor()
-				case 0x04:
-					// 8XY4 - set VX to the sum of VX and VY then set the carry flag as appropriate
-					c.setRegisterToSum()
-				case 0x05:
-					// 8XY5 - set VX to VX - VY then set the carry flag as appropriate
-					c.setRegisterToDifferenceA()
-				case 0x06:
-					// 8XY6 - set VX to VY (if CopyRegistersOnShift), shift the value of VX 1 bit right and set VF to
-					// the bit shifted out
-					c.shiftRight()
-				case 0x07:
-					// 8XY7 - set VX to VY - VX then set the carry flag as appropriate
-					c.setRegisterToDifferenceB()
-				case 0x0E:
-					// 8XYE - set VX to VY (if CopyRegistersOnShift), shift the value of VX 1 bit left and set VF to the
-					// bit shifted out
-					c.shiftLeft()
-				default:
-					panic(fmt.Errorf("UNHANDLED at %x: %x\n", c.pc, c.cir))
-				}
-
-			case 0x90:
-				// 9XYN - skip one if registers not equal
-				c.skipNotEqRegReg()
-
-			case 0xA0:
-				// ANNN - set index register to constant
-				c.setIndexRegister()
-
-			case 0xB0:
-				// BNNN - set PC to NNN + V0 - if VariableOffsetRegister, BXNN - set PC to XNN + VX
-				c.jumpWithOffset()
-
-			case 0xC0:
-				// CXNN - generate a random byte, AND it with NN and store in VX
-				c.random()
-
-			case 0xD0:
-				// DXYN - display
-				c.display()
-
-			case 0xE0:
-				// Input
-				switch c.cir[1] {
-				case 0x9E:
-					// EX9E - skip one if key with the value stored in VX is pressed
-					c.skipIfKey()
-				case 0xA1:
-					// EXA1 - skip one if key with the value stored in VX is not pressed
-					c.skipIfNotKey()
-				default:
-					panic(fmt.Errorf("UNHANDLED at %x: %x\n", c.pc, c.cir))
-				}
-
-			case 0xF0:
-				switch c.cir[1] {
-				case 0x07:
-					// FX07 - set value of VX to the current value of the delay timer
-					c.getDelayTimer()
-				case 0x15:
-					// FX15 - set delay timer to value of VX
-					c.setDelayTimer()
-				case 0x18:
-					// FX18 - set sound timer to the value of VX
-					c.setSoundTimer()
-				case 0x0A:
-					// FX0A - blocks until a key is pressed. Stores that key's value in VX then continues.
-					c.getPressedKey()
-				case 0x1E:
-					// FX1E - adds the value of VX to the index register and set VF accordingly if the index register
-					// "overflows" above 0x0FFF
-					c.addToIndexRegister()
-				case 0x29:
-					// FX29 - set the index register to the address of the hex character in VX
-					c.getFontCharacter()
-				case 0x33:
-					// FX33 - take the value of VX, converts it to a denary number and the put each individual digit in
-					// the memory location specified by the index register + the digit number
-					c.convertToDecimal()
-				case 0x55:
-					// FX55 - store the value of each general purpose register from V0 to VX inclusive in consecutive
-					// memory addresses starting from the current value of the index register
-					c.storeMemory()
-				case 0x65:
-					// FX65 - loads the value of each general purpose register from V0 to VX inclusive from consecutive
-					// memory addresses starting from the current value of the index register
-					c.loadMemory()
-				default:
-					panic(fmt.Errorf("UNHANDLED at %x: %x\n", c.pc, c.cir))
-				}
-
-			default:
-				panic(fmt.Errorf("UNHANDLED at %x: %x\n", c.pc, c.cir))
-			}
+			c.tick()
 
 		}
 	}
