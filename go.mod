0b6983d59674836dfcc508eea8a925a7365e3431
AKP <tom@tdpain.net>
2021-06-15 09:59:36 +0100

    Update build location and build script
    Signed-off-by: AKP <tom@tdpain.net>

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.16
 
 require (
 	github.com/alexflint/go-arg v1.4.2 // indirect
+	github.com/codemicro/alib-go v0.1.0 // indirect
 	github.com/hajimehoshi/ebiten/v2 v2.1.1 // indirect
+	github.com/magefile/mage v1.11.0 // indirect
 )
--- magefile.go
+++ magefile.go
@@ -3,9 +3,21 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/codemicro/alib-go/mage/exmg"
 	"github.com/magefile/mage/sh"
 )
 
 func Build() error {
-	return sh.Run("go", "build" "./...")
+	const buildPackage = "github.com/codemicro/chip8/cmd/chip8"
+
+	outputDir := filepath.Join("bin", fmt.Sprintf("%s-%s", exmg.GetTargetOS(), exmg.GetTargetArch()))
+	basePackageName := filepath.Base(buildPackage)
+
+	_ = os.MkdirAll(outputDir, os.ModeDir)
+
+	return sh.Run("go", "build", "-o", filepath.Join(outputDir, basePackageName), buildPackage)
 }
